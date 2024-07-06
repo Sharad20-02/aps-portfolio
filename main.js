ebb7011d215ac66c8809f72b74abd5bb64ccc2af
Sharad20-02 <142373977+Sharad20-02@users.noreply.github.com>
2024-07-06 13:23:35 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,58 @@
+document
+  .getElementById('whySpotify')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+    document
+      .getElementById('whySpotifyContent')
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+
+document
+  .getElementById('homeButton')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+    document
+      .getElementById('homeContent')
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+
+document
+  .getElementById('functionalityButton')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+    document
+      .getElementById('functionalityContent')
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+
+document
+    .getElementById('function1')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+        window.location.href =
+          'https://github.com/Sharad20-02/Basic-Implementation/blob/main/MergeSort.java';
+});
+
+document
+  .getElementById('function2')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+    window.location.href =
+      'https://github.com/Sharad20-02/Basic-Implementation/blob/main/FisherYatesShuffleAlgorithm.java';
+  });
+
+  document
+    .getElementById('function3')
+    .addEventListener('click', function (event) {
+      event.preventDefault();
+      window.location.href =
+        'https://github.com/Sharad20-02/Basic-Implementation/blob/main/LevenshteinDistance.java';
+    });
+    
+    document
+      .getElementById('function4')
+      .addEventListener('click', function (event) {
+        event.preventDefault();
+        window.location.href =
+          'https://github.com/Sharad20-02/Basic-Implementation/blob/main/SoundexSearch.java';
+      });
